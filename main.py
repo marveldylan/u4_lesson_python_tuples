2b299fe50eb08b25513a1c47be3b781a6d424d52
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-02 09:17:29 -0400

    completed lesson

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 def create_tuple(list_of_nums):
     # Create a tuple from a list and return it.
-    return
+    tup_of_nums = tuple(list_of_nums)
+    return tup_of_nums
 
 
 def get_value(tup):
     # Return the 3rd value in the provided tuple
-    return
+    return tup[2]
 
 
 def get_values(tup):
     # return the values from index 1-3
     # 3 should be inclusive
-    return
+    return tup[1:4]
 
 
 def get_max(tup):
     # return the largest number in the provided tuple
-    return
+    return max(tup)
 
